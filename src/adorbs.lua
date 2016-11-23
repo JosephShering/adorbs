04b9c6cc344e37845dd6864e0966ffa8a38523f4
JosephShering <joseph.shering@gmail.com>
2016-11-23 14:45:51 -0500

    initial commit

---
+++ src/adorbs.lua
@@ -0,0 +1,70 @@
+local lume = require 'lib/lume'
+
+local Adorbs = {}
+local engine = {}
+local state = {
+    resources = {
+        components = {},
+        systems = {}
+    }
+}
+
+function engine.create(stateLocation)
+    state = stateLocation
+
+    state.resources = {
+        components = {},
+        systems = {},
+        entities = {}
+    }
+
+    state.entities = {}
+end
+
+function engine.addSystem(name, system)
+    state.resources.systems[name] = system
+end
+
+function engine.removeSystem(name)
+
+end
+
+
+local component = {}
+function component.create(name, defaultFieldValues)
+    state.resources.components[name] = defaultFieldValues
+end
+
+local system = {}
+function system.create(name, logic)
+
+end
+
+local entity = {}
+function entity.create(name, components, isActive)
+    local newEntity = {
+        active = isActive,
+        components = {}
+    }
+
+    for name, component in pairs(components) do
+        if type(name) == "string" and type(component) == "table" then
+            newEntity.components[name] = component
+        else
+            print(name)
+            newEntity.components[component] = lume.clone(state.resources.components[component])
+        end
+    end
+
+    state.entities[name] = newEntity
+end
+
+function entity.addComponent(entity, component)
+end
+
+return {
+    engine = engine,
+    system = system,
+    entity = entity,
+    component = component
+}
---
+++ tests/main.lua
@@ -0,0 +1,22 @@
+local adorbs = require 'src/adorbs'
+local inspect = require 'lib/inspect'
+
+local engine = adorbs.engine
+local component = adorbs.component
+local entity = adorbs.entity
+
+local state = {}
+
+engine.create(state)
+component.create("Transform", {x = 0, y = 0})
+component.create("Character", { health = 100 })
+component.create("Body", { fixture = {}, body = {}, world = {}, scale = 1, angle = 0 })
+
+entity.create("Player", {
+    ["Transform"] = { x = 100, y = 100 },
+    "Character"
+})
+
+print(inspect(state))
+state.entities["Player"].active = true
+state.entities["Player"].components["Transform"].x = 10
