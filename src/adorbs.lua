7cc1ee605133bf1bc42f3b1ac9a1cbaeb76b8d47
JosephShering <joseph.shering@gmail.com>
2016-11-23 16:24:11 -0500

    added more features

--- src/adorbs.lua
+++ src/adorbs.lua
@@ -1,45 +1,57 @@
 local lume = require 'lib/lume'
+local inspect = require 'lib/inspect'
+local state = {}
 
-local Adorbs = {}
+-- Engine
 local engine = {}
-local state = {
-    resources = {
-        components = {},
-        systems = {}
-    }
-}
-
 function engine.create(stateLocation)
     state = stateLocation
 
     state.resources = {
         components = {},
-        systems = {},
-        entities = {}
+        systems = {}
     }
 
     state.entities = {}
+    state.systems = {}
 end
 
-function engine.addSystem(name, system)
-    state.resources.systems[name] = system
+function engine.addSystem(systemName)
+    local system, err = pcall(lume.clone, state.resources.systems[systemName])
+
+    if err then
+        print("No system called " .. systemName .. " in resources")
+    else
+        state.systems[systemName] = lume.clone(state.resources.systems[systemName])
+    end
 end
 
-function engine.removeSystem(name)
-
+function engine.removeSystem(systemName)
+    table.remove(state.systems[systemName])
 end
+-- end Engine
 
 
+-- Component
 local component = {}
 function component.create(name, defaultFieldValues)
     state.resources.components[name] = defaultFieldValues
 end
+-- end Component
 
+
+-- System
 local system = {}
-function system.create(name, logic)
-
+function system.create(name, components, updateFunc, drawFunc)
+    state.resources.systems[name] = {
+        update = updateFunc,
+        draw = drawFunc
+    }
 end
+-- end System
 
+
+-- Entity
 local entity = {}
 function entity.create(name, components, isActive)
     local newEntity = {
@@ -47,28 +59,37 @@ function entity.create(name, components, isActive)
         components = {}
     }
 
-    for name, component in pairs(components) do
-        if type(name) == "string" and type(component) == "table" then
-            newEntity.components[name] = component
-        else
-            print(name)
-            newEntity.components[component] = lume.clone(state.resources.components[component])
+    if components ~= nil then
+        for componentName, component in pairs(components) do
+            if type(component) == 'table' and lume.count(component) <= 0 then
+                print("You assigns the component " .. componentName .. " to " .. name .. " but it was empty")
+            end
+
+            if type(componentName) == "string" and type(component) == "table" then
+                newEntity.components[componentName] = component
+            else
+                local resComp, error = pcall(lume.clone, state.resources.components[component])
+
+                if error then
+                    print('No component called ' .. component .. ' in resources')
+                else
+                    newEntity.components[component] = resComp
+                end
+
+            end
         end
     end
 
     state.entities[name] = newEntity
+    return state.entities[name]
 end
 
-function entity.addComponent(entity, component)
+function entity.addComponent(entityName, componentName, componentOverride)
+    state.entities[entityName].components[componentName] = lume.clone(state.resources.components[componentName])
 end
+-- end Entity
+
 
 return function()
     return engine, system, entity, component
 end
-
--- return {
---     engine = engine,
---     system = system,
---     entity = entity,
---     component = component
--- }
--- tests/main.lua
+++ tests/main.lua
@@ -1,21 +1,30 @@
-local adorbs = require 'src/adorbs'
+local engine, system, entity, component = require 'src/adorbs' ()
 local inspect = require 'lib/inspect'
 
-local engine = adorbs.engine
-local component = adorbs.component
-local entity = adorbs.entity
-
 local state = {}
-
 engine.create(state)
-component.create("Transform", {x = 0, y = 0})
-component.create("Character", { health = 100 })
-component.create("Body", { fixture = {}, body = {}, world = {}, scale = 1, angle = 0 })
+component.create("Transform", { x = 0, y = 0 })
+component.create("Name", { displayName = "" })
 
 entity.create("Player", {
     ["Transform"] = { x = 100, y = 100 },
-    "Character"
+    ["Name"] = { displayName = "Joe" }
 })
 
-state.entities["Player"].active = true
-state.entities["Player"].components["Transform"].x = 10
+entity.create("Node")
+system.create(
+    "InputControl",
+    {"Character", "Transform"},
+    function(components, dt)
+        for _, component in pairs(components) do
+
+        end
+    end,
+    function()
+
+    end
+)
+
+engine.addSystem("InputControl")
+
+print(inspect(state))
